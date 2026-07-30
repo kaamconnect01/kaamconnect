94685c94f68ff9aeebfa6e310d3b4124a5d9556d
kaamconnect01 <manishpersonaluse@gmail.com>
2026-07-30 09:58:08 +0530

    Add files via upload

---
+++ static/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    
+    self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png'
+    });
+});
