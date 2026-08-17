6274df3031f92e0823553dc8cd585014536cc08d
kaamconnect01 <manishpersonaluse@gmail.com>
2026-08-17 11:51:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -415,6 +415,10 @@ def registered_shops():
     shops = User.query.filter_by(role='shop_owner').all()
     return render_template('shops.html', shops=shops)
 
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
 @app.route('/admin/toggle_block/<int:user_id>', methods=['POST'])
 @login_required
 def toggle_block_user(user_id):
